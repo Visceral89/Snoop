f3e99e813b8810ea00927c10f259e22cc6758202
Rasmus <rasmus.brem@gmail.com>
2022-03-14 15:48:38 +0100

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hej");
     }
 
     // Update is called once per frame
